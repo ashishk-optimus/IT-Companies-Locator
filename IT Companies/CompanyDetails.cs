a9e33dbc165f6e978eadb08a6fe98fb82b0e5976
Ashish Kumar <ascish@hotmail.com>
2018-02-27 15:19:26 +0530

    Modified CompanyDetails Class

--- IT Companies/CompanyDetails.cs
+++ IT Companies/CompanyDetails.cs
@@ -11,13 +11,25 @@ namespace IT_Companies
         private string name;
         private string address;
 
-        public string getName() => this.name;
+        public string getName()
+        {
+            return this.name;
+        }
 
-        public void setName(string name) => this.name = name;
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        
+        public string Address()
+        {
+            return this.address;
+        }
 
-        public string Address => this.address;
-
-        public void setAddress(string address) => this.address = address;
+        public void setAddress(string address)
+        {
+            this.address = address;
+        }
 
         public override string ToString()
         {
